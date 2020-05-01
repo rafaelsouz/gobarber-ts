da855b20a11f8cd9b1354eaac1b4916a8b5c3c9d
Rafael Souza <raffaelsouza908@gmail.com>
2020-04-30 21:42:12 -0300

    Fazendo autenticação do usuário

--- backend/src/routes/sessions.routes.ts
+++ backend/src/routes/sessions.routes.ts
@@ -1,12 +1,23 @@
 import { Router } from 'express';
 
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    return res.json({ ok: true });
+    const authenticateUser = new AuthenticateUserService();
+
+    const { user } = await authenticateUser.execute({
+      email,
+      password,
+    });
+
+    delete user.password;
+
+    return res.json({ user });
   } catch (err) {
     return res.status(400).json({ error: err.message });
   }
