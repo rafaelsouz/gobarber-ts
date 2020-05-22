cd500b947706ee85b262f51aaad5c0c766089255
Rafael Souza <raffaelsouza908@gmail.com>
2020-05-22 13:47:16 -0300

    Adicionando e removendo toast da tela

---
+++ frontend/src/components/ToastContainer/Toast/index.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect } from 'react';
+import {
+  FiAlertCircle,
+  FiXCircle,
+  FiInfo,
+  FiCheckCircle,
+} from 'react-icons/fi';
+
+import { ToastMessage, useToast } from '../../../hooks/toast';
+
+import { Container } from './styles';
+
+interface ToastProps {
+  message: ToastMessage;
+}
+
+const icons = {
+  info: <FiInfo size={24} />,
+  error: <FiAlertCircle size={24} />,
+  sucess: <FiCheckCircle size={24} />,
+};
+
+const Toast: React.FC<ToastProps> = ({ message }) => {
+  const { removeToast } = useToast();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      removeToast(message.id);
+    }, 3000);
+
+    // Essa função só será executada caso o componente deixe de existir(Quando o usuário clicar em remover o toast)
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [message.id, removeToast]);
+
+  return (
+    <Container type={message.type} hasDescription={!!message.description}>
+      {icons[message.type || 'info']}
+
+      <div>
+        <strong>{message.title}</strong>
+        {message.description && <p>{message.description}</p>}
+      </div>
+
+      <button onClick={() => removeToast(message.id)} type="button">
+        <FiXCircle size={18} />
+      </button>
+    </Container>
+  );
+};
+
+export default Toast;
