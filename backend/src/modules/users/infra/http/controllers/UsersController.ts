3d08ffc0ac09b22a8b6b3d1163b7e993e8b89bae
Rafael Souza <raffaelsouza908@gmail.com>
2020-06-08 20:20:27 -0300

    Adicionando controllers nas rotas

---
+++ backend/src/modules/users/infra/http/controllers/UsersController.ts
@@ -0,0 +1,18 @@
+import { container } from 'tsyringe';
+import { Request, Response } from 'express';
+
+import CreateUserService from '@modules/users/services/CreateUserService';
+
+export default class UsersController {
+  async create(req: Request, res: Response): Promise<Response> {
+    const { name, email, password } = req.body;
+
+    const createUser = container.resolve(CreateUserService);
+
+    const user = await createUser.execute({ name, email, password });
+
+    delete user.password;
+
+    return res.json(user);
+  }
+}
