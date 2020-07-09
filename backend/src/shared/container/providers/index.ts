6ce7f3cd9584f2bf18753e3c770e2efb77a9c22a
Rafael Souza <raffaelsouza908@gmail.com>
2020-07-09 20:49:03 -0300

    Envio de emails em ambiente de desenvolvimento

--- backend/src/shared/container/providers/index.ts
+++ backend/src/shared/container/providers/index.ts
@@ -3,7 +3,16 @@ import { container } from 'tsyringe';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
+import IMailProvider from './MailProvider/models/IMailProvider';
+import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
+
 container.registerSingleton<IStorageProvider>(
-  'IStorageProvider',
+  'StorageProvider',
   DiskStorageProvider,
+);
+
+// Criando uma instancia para disparar o constructor da classe
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  new EtherealMailProvider(),
 );
