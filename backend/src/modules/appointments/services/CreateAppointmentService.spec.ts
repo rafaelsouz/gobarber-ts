e39ef52d6e50098c9c24fcf781e253f3de6d006d
Rafael Souza <devrafaelsouza@gmail.com>
2020-07-23 12:20:14 -0300

    Chore: Adicionando o campo user_id nos test de criação de agendamento.

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -18,6 +18,7 @@ describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
     const appointment = await createAppointment.execute({
       date: new Date(),
+      user_id: '2313',
       provider_id: '2313',
     });
 
@@ -32,12 +33,14 @@ describe('CreateAppointment', () => {
 
     await createAppointment.execute({
       date: appointmentDate,
+      user_id: '2313',
       provider_id: '2313',
     });
 
     await expect(
       createAppointment.execute({
         date: appointmentDate,
+        user_id: '2313',
         provider_id: '2313',
       }),
     ).rejects.toBeInstanceOf(AppError);
