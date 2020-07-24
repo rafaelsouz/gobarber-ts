532a659a84951654f8b8ee03a1e8b5c78946be5e
Rafael Souza <devrafaelsouza@gmail.com>
2020-07-24 14:27:18 -0300

    Test: Listagem da agenda do prestador logado.

---
+++ backend/src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,41 @@
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/fakeAppointmentsRepository';
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderAppointmentsService: ListProviderAppointmentsService;
+
+describe('ListProviderAppointmentsService', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderAppointmentsService = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list the appointments on a specific day ', async () => {
+    const appointment1 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'user',
+      date: new Date(2020, 6, 20, 14, 0, 0),
+    });
+
+    const appointment2 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'user',
+      date: new Date(2020, 6, 20, 15, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 6, 20, 11).getTime();
+    });
+
+    const appointments = await listProviderAppointmentsService.execute({
+      provider_id: 'provider',
+      day: 20,
+      month: 7,
+      year: 2020,
+    });
+
+    expect(appointments).toEqual([appointment1, appointment2]);
+  });
+});
